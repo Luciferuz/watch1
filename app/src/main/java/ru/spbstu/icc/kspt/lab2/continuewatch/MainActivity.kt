b6a397f6555daab2f012f20e446d3c86fd13a7ad
Sergei Anciferov <sergozas@yandex.ru>
2021-11-26 22:51:31 +0300

    1

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -1,50 +1,62 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
+
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textSecElapsed: TextView
+    lateinit var sharedPref: SharedPreferences
+    lateinit var thread: Thread
+    var start: Long = 0
+    var end: Long = 0
     var secondsElapsed: Int = 0
-    lateinit var textSecondsElapsed: TextView
-    var onScreen = true
-
-    var backgroundThread = Thread {
-        while (true) {
-            if (onScreen) {
-                Thread.sleep(1000)
-                textSecondsElapsed.post {
-                    textSecondsElapsed.text = getString(R.string.seconds_elapsed, secondsElapsed++)
-                }
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        secondsElapsed = savedInstanceState?.getInt(SECONDS_ELAPSED) ?: 0
         setContentView(R.layout.activity_main)
-        textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
-        backgroundThread.start()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        onScreen = true
+        textSecElapsed = findViewById(R.id.textSecondsElapsed)
+        sharedPref = getSharedPreferences("Seconds elapsed", Context.MODE_PRIVATE)
+        val newTime: Int = sharedPref.getInt("Seconds elapsed", secondsElapsed)
+        textSecElapsed.text = getString(R.string.text_view, newTime)
     }
 
     override fun onStop() {
+        Log.i("TEST","STOPPED")
+        end = System.currentTimeMillis()
+        thread.interrupt()
+        secondsElapsed += ((end - start)/1000).toInt()
+        with(sharedPref.edit()) {
+            putInt("Seconds elapsed", secondsElapsed)
+            apply()
+        }
         super.onStop()
-        onScreen = false
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt(SECONDS_ELAPSED, secondsElapsed)
-        super.onSaveInstanceState(outState)
+    override fun onStart() {
+        Log.i("TEST","STARTED")
+        start = System.currentTimeMillis()
+        secondsElapsed = sharedPref.getInt("Seconds elapsed", secondsElapsed)
+        thread = Thread {
+            while (!Thread.currentThread().isInterrupted) {
+                try {
+                    Thread.sleep(1000)
+                    Log.i("TEST","RUNNING")
+                    textSecElapsed.post {
+                        val newTime = secondsElapsed + ((System.currentTimeMillis() - start)/1000)
+                        textSecElapsed.text = getString(R.string.text_view, newTime)
+                    }
+                } catch (e: InterruptedException) {
+                    Log.i("TEST","STOPPED")
+                    Thread.currentThread().interrupt()
+                }
+            }
+        }
+        thread.start()
+        super.onStart()
     }
-
-    companion object { const val SECONDS_ELAPSED = "Seconds elapsed" }
 
 }
